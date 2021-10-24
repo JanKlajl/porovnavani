15f2f378a76260b7e0e705776f990b9aa83ed426
majitel <majitel@GLEN>
2021-10-24 19:26:42 +0200

    Přidejte soubory projektu.

---
+++ porovnavaní/porovnavaní.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	int x = 1000;
+	int y = 924;
+	// ktere cislo je vetsi?
+	if (x > y) {
+		printf("cislo %d ", x);
+		printf("je vetsi nez cislo %d", y);
+	}
+
+	if (y > x) {
+		printf("cislo %d ", y);
+		printf("je vetsi nez cislo %d", x);
+	}
+}
